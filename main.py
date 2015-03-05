35efe395888f338a87c7d671129b7e81667810cb
Raj Kiran Grandhi <grajkiran@gmail.com>
2015-03-05 11:25:32 +0530

    Added option for blitting

--- main.py
+++ main.py
@@ -4,14 +4,22 @@ try:
     import tkinter as tk
 except ImportError:
     import Tkinter as tk
+import sys
+from optparse import OptionParser
 
 # Local imports
 from plotting import FigureWindow
 from gui import AppWindow
 from dynsystem import DynamicSystem
 
+
+parser = OptionParser()
+parser.add_option("-b", "--blit", action = "store_true", default = True)
+parser.add_option("-n", "--no-blit", action = "store_false", dest = "blit")
+opts, args = parser.parse_args()
+
 system = DynamicSystem('y', '-x + mu * (1 - x*x)*y', dict(mu = 1.0))
-fig = FigureWindow()
+fig = FigureWindow(blit = opts.blit)
 root = tk.Tk()
 root.protocol('WM_DELETE_WINDOW', root.quit)
 fig.bind('close_event', lambda evt: root.quit())
