cda323c453eafd34137532bde6faf0f5f09579bb
Raj Kiran Grandhi <grajkiran@gmail.com>
2015-05-16 19:05:38 +0530

    Bugfix 6.6: Put user defined model at the end

--- presets.py
+++ presets.py
@@ -22,17 +22,6 @@
 from collections import OrderedDict
 
 systems = OrderedDict()
-systems["User defined"] = {
-            "x": "0",
-            "y": "0",
-            "z": "0",
-            "xlim": [-5, 5],
-            "ylim": [-5, 5],
-            "zlim": [-5, 5],
-            "tmax": 25.0,
-            "dt": 0.05,
-            "projection": "3D",
-            }
 systems["Simple pendulum"] = {
             "x": "y",
             "y": "sin(x)",
@@ -100,6 +89,17 @@ systems["Rossler attractor"] = {
             "tmax": 100.0,
             "projection": "3D",
             "locations": [(-10.5, 10.5), (-10.5, 10.6)],
+            }
+systems["User defined"] = {
+            "x": "0",
+            "y": "0",
+            "z": "0",
+            "xlim": [-5, 5],
+            "ylim": [-5, 5],
+            "zlim": [-5, 5],
+            "tmax": 25.0,
+            "dt": 0.05,
+            "projection": "3D",
             }
 #systems["Duffing oscillator"] = {
 #                "x": "y",
